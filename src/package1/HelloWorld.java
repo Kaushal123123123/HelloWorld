5f3f14492ca748875eb0ab8c6f4cb9922299227f
Kaushal123123123 <Kaushal.k@selectiva.com>
2024-03-14 14:34:56 +0530

    Added line in helloworld.java

--- src/package1/HelloWorld.java
+++ src/package1/HelloWorld.java
@@ -4,6 +4,13 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("hello world");
+		int a=1;
+		int b=2;
+		int c=a*b;
+		int d=a/b;
+		System.out.println("Addition of two number is"+c);
+		System.out.println("Divided a / b"+d);
 	}
 
 }
